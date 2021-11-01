19cd7823d47c17d3d57832ee19f93c5f066003ae
Pandation <f.baumes@laposte.net>
2021-11-01 16:23:11 +0100

    clean code

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Header from "../components/Header";
-import Content from "../components/Content";
-import Footer from "../components/Footer";
-import Learning from "../components/Learning";
+import Header from "@/components/Header";
+import Content from "@/components/Content";
+import Footer from "@/components/Footer";
+import Learning from "@/components/Learning";
 
 export default function Home() {
   return (
